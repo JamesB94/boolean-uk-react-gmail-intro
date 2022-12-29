03bdea7b8044e0e947122be90a89c0d39d3766bb
James Byrne <james.c.byrne1994@gmail.com>
2022-12-29 14:45:41 +0000

    core criteria met

---
+++ src/Sidebar/sideBar.jsx
@@ -0,0 +1,32 @@
+import "./SideBar.css"
+
+const SideBar = () => {
+
+
+    return (  
+        <div className="sideBar">
+
+        <nav className="left-menu">
+            <ul className="inbox-list">
+            <li className="item active">
+                <span className="label">Inbox</span>
+                <span className="count">3</span>
+            </li>
+            <li className="item">
+                <span className="label">Starred</span>
+                <span className="count">2</span>
+            </li>
+
+            <li className="item toggle">
+                <label htmlFor="hide-read">Hide read</label>
+                <input id="hide-read" type="checkbox" checked={false} />
+            </li>
+            </ul>
+      </nav>
+
+
+        </div>
+    );
+}
+ 
+export default SideBar;
